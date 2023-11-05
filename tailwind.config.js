0317d4829a34615a6bee88742da514b99fcc579a
Yuri Pires <yuripiresbr@gmail.com>
2023-11-05 19:31:44 -0300

    chore: added styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -30,7 +30,13 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      colors: {
+        blackestBlue: "#1B262C",
+        deepBlue: "#0F4C75",
+        blue: "#3282B8",
+        lightBlue: "#BBE1FA",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
