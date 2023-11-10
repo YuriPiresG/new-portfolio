195d1de23dee3c0d3a9ef3b1d0c8495e16a1f009
Yuri Pires <yuripiresbr@gmail.com>
2023-11-10 11:20:15 -0300

    style: added new styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -16,11 +18,15 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        cityBg: "url('../assets/portBg.jpg')",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
+  
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
@@ -30,13 +36,36 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      boxShadow: {
+        glass: "0 4px 30px rgba(0, 0, 0, 0.1)",
+      },
+
       colors: {
         blackestBlue: "#1B262C",
         deepBlue: "#0F4C75",
         blue: "#3282B8",
         lightBlue: "#BBE1FA",
+        glass: "rgba(255, 255, 255, 0.08)",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    plugin(function ({ addComponents }) {
+      const newComponents = {
+        ".bg-glass-effect::before": {
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          "backdrop-filter": "blur(12px)",
+          "z-index": "-1",
+        },
+      };
+
+      addComponents(newComponents);
+    }),
+  ],
 };
