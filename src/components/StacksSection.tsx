c931f8831a7e90cac9ff028c8e75eb20bd21390e
Yuri Pires <yuripiresbr@gmail.com>
2023-11-18 11:06:23 -0300

    feat: Created stack section

---
+++ src/components/StacksSection.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+import { SiNestjs, SiPostgresql } from "react-icons/si";
+import {
+  TbBrandHtml5,
+  TbBrandTailwind,
+  TbBrandVite,
+  TbBrandReact,
+  TbBrandNextjs,
+  TbBrandTypescript,
+  TbBrandNodejs,
+  TbBrandPrisma,
+} from "react-icons/tb";
+
+export default function StacksSection() {
+  return (
+    <section className="group relative col-span-2 grid grid-cols-5 items-center justify-center gap-3 rounded-2xl border-none bg-glass p-6 shadow-glass">
+      <h3 className="col-span-5 text-xl font-bold text-olive">Stacks</h3>
+      <h6 className="col-span-5 text-white">
+        Tecnologias que utilizo e tenho experiência
+      </h6>
+
+      <TbBrandHtml5 className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+      <TbBrandTailwind className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+      <TbBrandVite className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+
+      <TbBrandReact className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+      <TbBrandNextjs className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+      <TbBrandTypescript className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+      <TbBrandNodejs className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+      <SiNestjs className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+      <TbBrandPrisma className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+      <SiPostgresql className="h-full w-full text-white opacity-80 transition-opacity ease-in-out hover:opacity-100 " />
+    </section>
+  );
+}
